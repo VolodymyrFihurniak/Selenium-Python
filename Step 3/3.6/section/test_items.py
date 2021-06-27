632f5a52a26a75d8cfa18c038a91c0db683d593b
Volodymyr Fihurniak <volodymyr.fihurniak@gmail.com>
2021-06-27 15:47:36 +0300

    Add fix to step 3/3.6/section

--- Step 3/3.6/section/test_items.py
+++ Step 3/3.6/section/test_items.py
@@ -1,5 +1,11 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
 def test_guest_should_see_login_link(browser):
     browser.get('http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/')
     browser.implicitly_wait(10)
     browser.find_element_by_xpath('//button[@class="btn btn-lg btn-primary btn-add-to-basket"]').submit()
-    assert 'Coders at Work' in browser.find_element_by_class_name('alertinner').text, 'Error adding item to cart'
+    value = WebDriverWait(browser, 10).until(EC.visibility_of_element_located((By.CLASS_NAME, 'alertinner')))
+    assert 'Coders at Work' in value.text, 'Error adding item to cart'
