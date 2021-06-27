59ea113988d80d2f460a16c88fad98a49393e7bd
Volodymyr Fihurniak <volodymyr.fihurniak@gmail.com>
2021-06-27 16:02:00 +0300

    Add rename method to step 3/3.6/section

--- Step 3/3.6/section/test_items.py
+++ Step 3/3.6/section/test_items.py
@@ -3,7 +3,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 
-def test_guest_should_see_login_link(browser):
+def test_add_to_cart_button_is_displayeds(browser):
     browser.get('http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/')
     browser.implicitly_wait(10)
     browser.find_element_by_xpath('//button[@class="btn btn-lg btn-primary btn-add-to-basket"]').submit()
